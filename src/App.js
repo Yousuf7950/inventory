9cb5e4b75c2feaafe39ce32ce9c5fe3c076bb05f
Yousuf79500 <ysufsheikh@gmail.com>
2022-02-21 03:27:16 +0500

    Han bhaii
    edited , date time and dropdowns not working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import react from 'react'
 // bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddInventory from './components/classComponent';
 import AddProduct from './components/funcComponent';
 import { Header } from './components/Header';
 import Sidebar from './components/Sidebar';
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
       <div id="page-wrap" style={{margin:"1%"}}>
         <h1 style={{marginBottom:"30px"}}>Product Inventory form </h1>
         <AddInventory/>
+        <div><hr/><hr/></div>
         <AddProduct/>
         
         </div>
